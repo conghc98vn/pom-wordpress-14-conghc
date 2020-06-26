75b30c3b91fb1b7d41a8b56780e9eb58ee438ec0
Chi Cong <cong.hoang@vexere.com>
2020-06-26 15:40:34 +0700

    update tp 40 - 2

---
+++ interfaces/pageUI/bankGuru/AbstractPage.java
@@ -0,0 +1,24 @@
+package pageUI.bankGuru;
+
+public class AbstractPage {
+	// Textbox
+	public static final String DYNAMIC_TEXTBOX = "//input[@type='text' and @name='%s']";
+	
+	// Textarea
+	public static final String DYNAMIC_TEXTAREA = "//textarea[@name='%s']";
+	
+	// Radio Button
+	public static final String DYNAMIC_RADIO_BUTTON = "//input[@type='radio' and value='%s']";
+	
+	// Menu Link
+	public static final String DYNAMIC_LINK = "//ul[@class='menusubnav']//a[text()='%s']";
+	
+	// Button
+	public static final String DYNAMIC_BUTTON = "//input[@value='%s']";
+	
+	// Message
+	public static final String DYNAMIC_MESSAGE = "//p[@class='heading3' and text()='%s'";
+	
+	// Column Value
+	public static final String DYNAMIC_VALUE_BY_COLUMN_NAME = "//td[contains(text(), %s)]/following-sibling::td";
+}
